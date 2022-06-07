ca682ca9e13381e5c092eab0f5583ba5b8f3d2d6
rafsalas19 <rafsalas1987@hotmail.com>
2022-06-07 19:51:04 +0000

    added net host flag to docker run to allow for proper host name resolution. Modified the Moneo worker remote path to be tmp directory in the event of using a shared home drive. Reverted grafana config back to orignal

--- src/master/run.sh
+++ src/master/run.sh
@@ -4,7 +4,7 @@
 mkdir -m 777 /mnt/prometheus
 docker rm -f prometheus || true
 docker run --name prometheus \
-    -it -d -p 9090:9090 \
+    -it --net=host -d -p 9090:9090 \
     -v /mnt/prometheus:/prometheus \
     -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
     prom/prometheus \
@@ -17,7 +17,7 @@ docker run --name prometheus \
 # start grafana
 docker rm -f grafana || true
 docker run --name grafana \
-    -it -d -p 3000:3000 \
+    -it --net=host  -d -p 3000:3000 \
     --env-file $PWD/grafana/grafana.env \
     -v $PWD/grafana/dashboards:/var/lib/grafana/dashboards \
     -v $PWD/grafana/provisioning:/etc/grafana/provisioning \
