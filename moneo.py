5f5ad4833c4e8ad06930f99fd0cbec5561f95a7d
Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-07-11 16:04:54 +0000

    Adding start/stop and other cli options to moneo

--- moneo.py
+++ moneo.py
@@ -6,20 +6,31 @@ import os
 
 def deploy(args):
     dep_cmd='ansible-playbook -i ' + args.host_ini + ' src/ansible/deploy.yaml'
-    if args.skip_install :
+    
+    if args.deploy == 'workers':
+        dep_cmd= dep_cmd + ' -e "skip_master=true"'
+    if args.deploy == 'master' :
         dep_cmd= dep_cmd + ' -e "skip_worker=true"'
+    
     dep_cmd= dep_cmd + ' -e "job_Id=' + str(args.job_id) + '"'
     print(dep_cmd)
+    print('Deployment type: ' + args.deploy)
     os.system(dep_cmd)
     
 def stop(args):
     while(True) :
         confirm = input("Are you sure you would like to stop Moneo? (Y/n)\n")
+        
         if confirm.upper() == 'Y' :    
             dep_cmd='ansible-playbook -i ' + args.host_ini + ' src/ansible/shutdown.yaml'
+            if args.shutdown=='workers':
+                dep_cmd= dep_cmd + ' -e "skip_master=true"'
+            if args.shutdown == 'manager':
+                dep_cmd= dep_cmd + ' -e "skip_worker=true"'
             os.system(dep_cmd)
             print("Moneo is Shutting down \n")
             return 0
+        
         elif confirm.upper() == 'N':
             print("Canceling request to shutdown Moneo \n")
             return 0
@@ -27,17 +38,18 @@ def stop(args):
             print("Input not recognized\n")
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d','--deploy',action='store_true',help='Deploy Moneo. This command requires a config file be provided using --host_ini/-c option to specify file. If one is not provided it will attempt to use "host.ini" file in the Moneo directory by default. Moneo will fail to deploy if a file is not present.')
-    parser.add_argument('-s','--shutdown',action='store_true', help='Shutdown Moneo. This command requires a config file be provided using --host_ini/-c option to specify file. If one is not provided it will attempt to use "host.ini" file in the Moneo directory by default. Moneo will fail to shutdown if a file is not present.')
-    parser.add_argument('-c','--host_ini',default='./host.ini', help='Provide filepath and name of ansible config file. The default is host.ini in the Moneo directory.' )
+    parser = argparse.ArgumentParser(description='Moneo CLI')  
+    parser.add_argument('-d','--deploy' ,choices=['manager', 'workers', 'full'],help=' Deployment choices: {manager,workers,full}. Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument('-s','--shutdown',choices=['manager', 'workers', 'full'], help='Shutdown choices: {manager,workers,full}.  Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument('-c','--host_ini',default='./host.ini', help='Provide filepath and name of ansible config file. The default is host.ini in the Moneo directory.' )    
     parser.add_argument('-j','--job_id',default=-999, help='Job ID for filtering metrics by job group.' )
-    parser.add_argument('-k','--skip_install',action='store_true',help='skips instalation of Moneo software on the workers during deployment time')
     args = parser.parse_args()
     
     if(args.deploy and args.shutdown):
-        print("deploy and shutdown are exclusive arguments. Please only provide one.")
-        exit(1)
+         print("deploy and shutdown are exclusive arguments. Please only provide one.")
+         exit(1)
     elif(args.deploy):
         if (not os.path.isfile(args.host_ini)) :
             print(args.host_ini + " does not exist. Please provide a a host file. i.e. host.ini.")
--- src/worker/shutdown.sh
+++ src/worker/shutdown.sh
@@ -3,17 +3,17 @@
 arch=$1
 
 if [ $arch == "nvidia" ]; then
-	pkill -f "nvidia_exporter.py$" && sleep 1
-	pkill -f "net_exporter.py$" && sleep 1
+	pkill -f "nvidia_exporter.py*" && sleep 1
+	pkill -f "net_exporter.py*" && sleep 1
 	pkill -f "^nv-hostengine"
+	sleep 3
 	echo 0 >&2
 elif [ $arch == "amd" ]; then
-	pkill -f "amd_exporter.py$" && sleep 1
-	pkill -f "net_exporter.py$" && sleep 1
+	pkill -f "amd_exporter.py*" && sleep 1
+	pkill -f "net_exporter.py*" && sleep 1
 	pkill -f "^/opt/rocm/rdc/bin/rdcd"
+	sleep 3
 	echo 0 >&2
 else
 	echo "Proper architecture not provided. Choices are nvidia and amd."
 fi
-
-echo 1 >&2
