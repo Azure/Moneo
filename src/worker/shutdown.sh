108d4769e036e9effeef43e0d9c9adbfb2a129b0
Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-07-13 21:31:23 +0000

    additional tests added to ansible test to check that docker and exporter procs are deployed and terminated during deploy/shutdown. Also slight change to shutdown.sh for cleaner look

--- src/worker/shutdown.sh
+++ src/worker/shutdown.sh
@@ -7,13 +7,13 @@ if [ $arch == "nvidia" ]; then
 	pkill -f "net_exporter.py*" && sleep 1
 	pkill -f "^nv-hostengine"
 	sleep 3
-	echo 0 >&2
+	exit 0
 elif [ $arch == "amd" ]; then
 	pkill -f "amd_exporter.py*" && sleep 1
 	pkill -f "net_exporter.py*" && sleep 1
 	pkill -f "^/opt/rocm/rdc/bin/rdcd"
 	sleep 3
-	echo 0 >&2
+	exit 0
 else
 	echo "Proper architecture not provided. Choices are nvidia and amd."
 fi
