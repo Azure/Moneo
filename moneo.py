9afaf4b88775736d96ba3dfa094538a3c6e4550b
Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-08-03 18:11:41 +0000

    modified cli to allow for default shutdown/deploy options

--- moneo.py
+++ moneo.py
@@ -7,23 +7,23 @@ import os
 def deploy(args):
     dep_cmd='ansible-playbook -i ' + args.host_ini + ' src/ansible/deploy.yaml'
     
-    if args.deploy == 'workers':
+    if args.type == 'workers':
         dep_cmd= dep_cmd + ' -e "skip_master=true"'
-    if args.deploy == 'master' :
+    if args.type == 'master' :
         dep_cmd= dep_cmd + ' -e "skip_worker=true"'
     
-    print('Deployment type: ' + args.deploy)
+    print('Deployment type: ' + args.type)
     os.system(dep_cmd)
     
 def stop(args):
     while(True) :
-        confirm = input("Are you sure you would like to stop Moneo? (Y/n)\n")
+        confirm = input("Are you sure you would like to perform a '"+ args.type +"' shutdown of Moneo? (Y/n)\n")
         
         if confirm.upper() == 'Y' :    
             dep_cmd='ansible-playbook -i ' + args.host_ini + ' src/ansible/shutdown.yaml'
-            if args.shutdown=='workers':
+            if args.type=='workers':
                 dep_cmd= dep_cmd + ' -e "skip_master=true"'
-            if args.shutdown == 'manager':
+            if args.type == 'manager':
                 dep_cmd= dep_cmd + ' -e "skip_worker=true"'
             os.system(dep_cmd)
             print("Moneo is Shutting down \n")
@@ -35,12 +35,29 @@ def stop(args):
         else:
             print("Input not recognized\n")
 
+def check_deploy_shutdown(args,parser):
+    if (not os.path.isfile(args.host_ini)) :
+        print(args.host_ini + " does not exist. Please provide a host file. i.e. host.ini.")
+        parser.print_help()
+        exit(1)
+    choices=['manager', 'workers', 'full']
+    if(args.type[0] not in choices):
+        print('Deployment/shutdown type not recognized or entered defaulted to the full option.')
+        args.type = 'full'
+    args.type = args.type[0]
+
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Moneo CLI',prog='moneo.py',usage='%(prog)s [-d {manager,workers,full}] [-c HOST_INI] \nusage: %(prog)s [-s {manager,workers,full}] [-c HOST_INI]')  
-    parser.add_argument('-d','--deploy' ,choices=['manager', 'workers', 'full'],help=' Deployment choices: {manager,workers,full}. Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
-    parser.add_argument('-s','--shutdown',choices=['manager', 'workers', 'full'], help='Shutdown choices: {manager,workers,full}.  Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser = argparse.ArgumentParser(description='Moneo CLI',prog='moneo.py',usage='%(prog)s [-d ] [-c HOST_INI] [{manager,workers,full}] \
+    \nusage: %(prog)s [-s ] [-c HOST_INI] [{manager,workers,full}] \
+    \ni.e. python3 moneo.py -d -c ./host.ini full')  
+
     parser.add_argument('-c','--host_ini',default='./host.ini', help='Provide filepath and name of ansible config file. The default is host.ini in the Moneo directory.' )    
+    
+    parser.add_argument('-d','--deploy', action='store_true',help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument('-s','--shutdown',action='store_true', help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument('type', metavar='type', type=str,default=['full'], nargs="*", help='Type of deployment/shutdown. Choices: {manager,workers,full}. Default: full.')    
+
     args = parser.parse_args()
     
     if(args.deploy and args.shutdown):
@@ -48,16 +65,10 @@ if __name__ == '__main__':
          parser.print_help()
          exit(1)
     elif(args.deploy):
-        if (not os.path.isfile(args.host_ini)) :
-            print(args.host_ini + " does not exist. Please provide a a host file. i.e. host.ini.")
-            parser.print_help()
-            exit(1)
+        check_deploy_shutdown(args,parser)
         deploy(args)
-    elif(args.shutdown):
-        if (not os.path.isfile(args.host_ini)) :
-            print(args.host_ini + " does not exist. Please provide a host file. i.e. host.ini.")
-            parser.print_help()
-            exit(1)    
+    elif(args.shutdown):  
+        check_deploy_shutdown(args,parser)
         stop(args)
     else:
         parser.print_help()
