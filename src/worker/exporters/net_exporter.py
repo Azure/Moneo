28b6eae38c08857e63b3134a0584e67aa2dc729a
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2022-08-05 15:00:20 -0500

    Unit tests (#10)
    * unit test helper and dat files
    
    * add command parser for net export. Add Unit test for cli,ansible,exporters
    
    * removing host.ini file. Adding timeout message to unit test subprocess helper. Adding new line to files where necessary
    
    Co-authored-by: Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>

--- src/worker/exporters/net_exporter.py
+++ src/worker/exporters/net_exporter.py
@@ -3,6 +3,7 @@ import sys
 import time
 import signal
 import logging
+import argparse
 
 import prometheus_client
 
@@ -139,9 +140,26 @@ def init_signal_handler():
     signal.signal(signal.SIGINT, exit_handler)
     signal.signal(signal.SIGTERM, exit_handler)
 
+def get_log_level(loglevel):
+    levelStr = loglevel.upper()
+    if levelStr == '0' or levelStr == 'CRITICAL':
+        numeric_log_level = logging.CRITICAL
+    elif levelStr == '1' or levelStr == 'ERROR':
+        numeric_log_level = logging.ERROR
+    elif levelStr == '2' or levelStr == 'WARNING':
+        numeric_log_level = logging.WARNING
+    elif levelStr == '3' or levelStr == 'INFO':
+        numeric_log_level = logging.INFO
+    elif levelStr == '4' or levelStr == 'DEBUG':
+        numeric_log_level = logging.DEBUG
+    else:
+        print ("Could not understand the specified --log-level '%s'" % (args.loglevel))
+        args.print_help()
+        sys.exit(2)
+    return numeric_log_level
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+def main(args):
+    logging.basicConfig(level=get_log_level(args.log_level))
     init_config()
     init_infiniband()
     init_signal_handler()
@@ -151,4 +169,13 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_level", default='INFO', help='Specify a log level to use for logging. CRITICAL (0) - \
+                        log only critical errors that drastically affect \
+                        execution ERROR (1) - Log any error in execution \
+                        WARNING (2) - Log all warnings and errors that occur \
+                        INFO (3) - Log informational messages about program \
+                        execution in addition to warnings and errors DEBUG (4) \
+                        - Log debugging information in addition to all')
+    args = parser.parse_args()
+    main(args)
---
+++ test/helper/shell_process.py
@@ -0,0 +1,15 @@
+import subprocess
+import shlex
+
+
+def shell_cmd(args, timeout):
+    """Helper Function for running subprocess"""
+    child = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    try:
+        result, errs = child.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:       
+        child.kill()
+        print("Command " + " ".join(args) + ", Failed on timeout")
+        result = 'TimeOut'
+        return result
+    return result.decode()
