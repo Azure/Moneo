ebe30f8edfe45e667e6aaa0e38b7f0cd5d3acdda
Lucas Morais Henrique <t-lucasmo@microsoft.com>
2022-08-08 18:31:12 -0400

    Remove dashboard and tests

--- test/helper/json_helper.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def jsonToDict(filepath: str):
-        with open(filepath) as response_data_file:
-            response_data = json.load(response_data_file)
-        return response_data
