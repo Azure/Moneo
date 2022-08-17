2fa6aa730cb33304839e6a1cf10e8e1c7f99a2b3
rafsalas19 <rafsalas1987@hotmail.com>
2022-08-17 17:29:05 +0000

    change test directory name

---
+++ tests/test_ansible.py
@@ -0,0 +1,83 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import unittest
+import shlex
+from helper import shell_process
+import re
+
+"""Moneo ansible single node test"""
+
+
+class AnsibleTestCase(unittest.TestCase):
+    """Ansible unit test class"""
+    
+    def check_docker_status(self):
+        """Helper to check docker container status"""
+        cmd='sudo docker container ls'
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args,15)
+        return ('grafana' in result), ( 'prometheus' in result )
+   
+    def check_exporter_status(self):
+        """Helper to check exporter status"""
+        cmds=['ps -f -C  python2','ps -f -C  python3']
+        expected=['nvidia_exporter.py', 'net_exporter.py']
+        
+        for i in range(len(cmds)):
+            args = shlex.split(cmds[i])
+            result = shell_process.shell_cmd(args,15)
+            if expected[i] not in result:
+                return False
+        return True
+    
+    def test_ansible_good_output(self):
+        """Test ansible deployment and shutdown output"""
+        cmds = ['ansible-playbook -i data/host.ini ../src/ansible/deploy.yaml','ansible-playbook -i data/host.ini ../src/ansible/shutdown.yaml']        
+        expected= ['failed=0','localhost'] #used for correct ansible output
+        #test successful deploy and shutdown output
+        for i in range(len(cmds)):
+            args = shlex.split(cmds[i])
+            result = shell_process.shell_cmd(args,120)
+            for exp in expected:
+                assert(exp in result) #check that ansible output has printed the correct output based on expected
+                
+    def test_ansible_deployed(self):
+        """Test ansible deployed containers/exporters"""
+        cmd = 'ansible-playbook -i data/host.ini ../src/ansible/deploy.yaml'        
+        #test successful deploy by looking at docker
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args,60)
+        #checking docker containers up
+        graf_status, prom_status =self.check_docker_status()
+        assert(graf_status)
+        assert(prom_status)
+        #checking exporters launched
+        exporter_status=self.check_exporter_status()
+        assert(exporter_status)
+        
+    def test_ansible_shutdown(self):
+        """Test ansible shutdown containers/exporters"""
+        cmd = 'ansible-playbook -i data/host.ini ../src/ansible/shutdown.yaml'      
+        #test successful deploy by looking at docker
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args,60)
+        #checking docker containers down
+        graf_status, prom_status =self.check_docker_status()
+        self.assertFalse(graf_status)
+        self.assertFalse(prom_status)
+        #checking exporters killed
+        exporter_status=self.check_exporter_status()
+        self.assertFalse(exporter_status)
+    
+    def test_ansible_bad_hostfile(self):
+        """Test ansible when give bad input file"""
+        cmd = 'ansible-playbook -i doesnotexist.ini ../src/ansible/shutdown.yaml'
+        expected=['Unable to parse','no hosts matched']
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args,15)
+        assert(expected[0] in result)
+        assert(expected[1] in result)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_cli.py
@@ -0,0 +1,74 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import unittest
+import shlex
+from helper import shell_process
+from helper import file_util
+import os
+import re
+from pathlib import Path
+"""Moneo CLI test"""
+
+
+class CLITestCase(unittest.TestCase):
+    """CLI unit test Class."""
+        
+    def test_cli_args(self):
+        """Test bad cli input"""
+        
+        #change directory to Moneo test directory
+        cdir=os.path.dirname(__file__)
+        if(cdir):
+            os.chdir(cdir)    
+            
+        cmd="python3 ../moneo.py "
+        
+        test_cases=[
+            '', #noArg
+            '-h', #help
+            '-d -s', # deploy and shutdown
+            '--notTheRightOption', #unrecognized input
+            '-d notTheRightOption', #bad choice for deploy
+            '-s full -c /tmp/thisfiledoesnotexist.txt' #unrecognized config
+        ]
+       # print(os.getcwd())
+
+        #test 1 no arg
+        testData = 'Moneo CLI Help Menu'
+        args = shlex.split(cmd + test_cases[0])
+        result = shell_process.shell_cmd(args,15)
+        assert(testData in result)
+        
+        #test 2 help 
+        testData = 'Moneo CLI Help Menu'
+        args = shlex.split(cmd + test_cases[1])
+        result = shell_process.shell_cmd(args,15)
+        assert(testData in result)
+        
+        #test 3 deploy and shutdown simultaneously
+        testData ='deploy and shutdown are exclusive arguments. Please only provide one.'
+        args = shlex.split(cmd +'-c data/host.ini '+ test_cases[2])
+        result = shell_process.shell_cmd(args,15)
+        assert(testData in result)
+        
+        #test 4 unrecognized input
+        testData ='error: unrecognized arguments'
+        args = shlex.split(cmd + test_cases[3])
+        result = shell_process.shell_cmd(args,15)
+        assert(testData in result)
+        
+        #test 5 deploy wrong option
+        testData ='Deployment/shutdown type not recognized or entered defaulted to the full option.'
+        args = shlex.split(cmd + '-c data/host.ini ' + test_cases[4])#deploy
+        result = shell_process.shell_cmd(args,60)        
+        assert(testData in result)
+        
+        #test 6 incorrect host config file path
+        testData ='does not exist. Please provide a host file.'
+        args = shlex.split(cmd + test_cases[5])
+        result = shell_process.shell_cmd(args,15)
+        assert(testData in result)
+ 
+if __name__ == '__main__':
+    unittest.main()
