6708889f5dc8d5d28a20fd7bd32226b92c98f7d0
Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-08-18 20:56:41 +0000

    modified unit tests and test data for new metrics. Small fix to job ID update yaml.

--- tests/test_nv_exporter.py
+++ tests/test_nv_exporter.py
@@ -18,10 +18,23 @@ class NvidiaExporterTestCase(unittest.TestCase):
         #running nvidia exporter for 5 seconds with debug log level. It will out put the metrics monitored
         cmd='timeout 5  python2 ' + path + '../src/worker/exporters/nvidia_exporter.py --log-level DEBUG'
         args = shlex.split(cmd)
-        result = shell_process.shell_cmd(args,15)
+        result = shell_process.shell_cmd(args,20)
         for metric in metricData:
             assert(metric in result)
-        
+
+    def test_metrics_prof(self):
+        """Check that required metrics are being tracked"""
+        path=os.path.dirname(__file__)
+        if path:
+            path=path + '/'
+        metricData= file_util.load_data(path+'data/nv_metric2.txt')
+        metricData = metricData.split("\n")
+        #running nvidia exporter for 5 seconds with debug log level. It will out put the metrics monitored
+        cmd='timeout 5  python2 ' + path + '../src/worker/exporters/nvidia_exporter.py -m --log-level DEBUG'
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args,20)
+        for metric in metricData:
+            assert(metric in result)        
     
         
         
