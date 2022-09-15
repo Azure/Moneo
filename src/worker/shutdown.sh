cb36c5d348da1ca39ef11704a2392a7ac0fbb69d
Rafsalas19 <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-09-15 16:25:50 +0000

    Refactor shutdown.sh to remove duplicate line of code

--- src/worker/shutdown.sh
+++ src/worker/shutdown.sh
@@ -2,22 +2,23 @@
 
 arch=$1
 
-if [ $arch == "nvidia" ]; then
-	pkill -f "nvidia_exporter.py*" && sleep 1
+kill_exporters() {
+    pkill -f "${1}_exporter.py*" && sleep 1
 	pkill -f "net_exporter.py*" && sleep 1
 	pkill -f "node_exporter.py*" && sleep 1
+}
+
+if [ $arch == "nvidia" ]; then
+	kill_exporters $arch
 	pkill -f "^nv-hostengine"
 	sleep 3
 	exit 0
 elif [ $arch == "amd" ]; then
-	pkill -f "amd_exporter.py*" && sleep 1
-	pkill -f "net_exporter.py*" && sleep 1
-	pkill -f "node_exporter.py*" && sleep 1
+        kill_exporters $arch
 	pkill -f "^/opt/rocm/rdc/bin/rdcd"
 	sleep 3
-	exit 0
+        exit 0
 else
 	echo "Proper architecture not provided. Choices are nvidia and amd."
 fi
-
-exit 0
+	exit 0
