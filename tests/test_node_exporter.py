20c6243d6384994d5fd508e4b1217520109cac98
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2022-09-15 16:54:26 -0400

    Adding Node exporter (#20)
    * prototyping a base node exporter
    
    * completed example for base/node exporter
    
    * Modifications made to integrate the node exporter to the rest of the deploy,update, and shutdown prometheus and ansible scripts
    
    * Adding comments to base and node exporter
    
    * Add comments and small modifications to conditional statements
    
    * adding comments
    
    * added unit test for node explorer and fixed lint issues on files
    
    * restructure docs
    
    * Update document paths
    
    * Exporting custom metrics documentation
    
    * Update README.md
    
    * Refactor shutdown.sh to remove duplicate line of code
    
    * Fix typos
    
    * adding more comments to guide users
    
    * Added overview of the node exporter
    
    Co-authored-by: Ubuntu <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>

---
+++ tests/test_node_exporter.py
@@ -0,0 +1,53 @@
+import unittest
+import shlex
+from helper import shell_process
+import os
+import sys
+
+p = os.path.abspath('../src/worker/exporters')
+sys.path.insert(1, p)
+
+from base_exporter import BaseExporter
+
+
+"""Moneo Node Exporter test"""
+
+
+class NodeExporterTestCase(unittest.TestCase):
+    """Node Exporter unit test Class."""
+    def test_base_class(self):
+        """test that functions in this class aren't implemented"""
+        fields = ['net_rx']
+        config = {'listen_port': 8003}  # dummy
+        exporter = BaseExporter(fields, config)
+        test = False
+
+        try:
+            exporter.collect(fields[0])  # 'net_rx'=dummy
+        except NotImplementedError:
+            test = True
+        assert (test)
+        test = False
+
+        try:
+            exporter.cleanup()
+        except NotImplementedError:
+            test = True
+        assert (test)
+
+    def test_metrics(self):
+        """Check that required metrics are being tracked"""
+        path = os.path.dirname(__file__)
+        if path:
+            path = path + '/'
+        metricData = ['net_rx', 'net_tx']
+        # running node exporter for 5 seconds with debug log level. It will out put the metrics monitored
+        cmd = 'timeout 5  python3 ' + path + '../src/worker/exporters/node_exporter.py --log_level DEBUG'
+        args = shlex.split(cmd)
+        result = shell_process.shell_cmd(args, 20)
+        for metric in metricData:
+            assert (metric in result)
+
+
+if __name__ == '__main__':
+    unittest.main()
