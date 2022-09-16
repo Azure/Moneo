8050b8a110bf81a5737ea2302b8ffe850b950db1
Rafsalas19 <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-09-16 15:50:31 +0000

    Modifying stderr stdout pipe and adding removal of automaticly generated host.ini file at the end of epilog script

--- examples/slurm/epilogMoneo.sh
+++ examples/slurm/epilogMoneo.sh
@@ -3,7 +3,9 @@
 echo $SLURM_JOB_ID > /tmp/testfile.txt
 
 # The command below pipes stdout/stderr to files, This is necessary for ansible to behave correctly.
-sudo -u <Your user> -H sh -c "cd  <Moneo DIR> &&  python3 moneo.py -j None -c /tmp/jobid_$SLURM_JOB_ID.txt  >> /tmp/moneolog.out  2>> /tmp/moneolog.err "
+sudo -u <Your user> -H sh -c "cd  <Moneo DIR> &&  python3 moneo.py -j None -c /tmp/jobid_$SLURM_JOB_ID.txt  >> /tmp/$SLURM_JOB_ID_moneolog.out 2>&1"
+
+rm /tmp/jobid_$SLURM_JOB_ID.txt
 
 exit 0
 
--- examples/slurm/prologMoneo.sh
+++ examples/slurm/prologMoneo.sh
@@ -4,7 +4,7 @@ echo "[worker]" > /tmp/jobid_$SLURM_JOB_ID.txt
 echo $SLURM_JOB_NODELIST >> /tmp/jobid_$SLURM_JOB_ID.txt
 
 # The command below pipes stdout/stderr to files, This is necessary for ansible to behave correctly. 
-sudo -u <Your user> -H sh -c "cd  <Moneo Dir> &&  python3 moneo.py -j $SLURM_JOB_NAME.$SLURM_JOB_ID -c /tmp/jobid_$SLURM_JOB_ID.txt  > /tmp/moneolog.out  2> /tmp/moneolog.err "
+sudo -u <Your user> -H sh -c "cd  <Moneo Dir> &&  python3 moneo.py -j $SLURM_JOB_NAME.$SLURM_JOB_ID -c /tmp/jobid_$SLURM_JOB_ID.txt  > /tmp/moneolog.out 2>&1"
 
 exit 0
 
