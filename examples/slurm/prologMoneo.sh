bc8680d11ca167c4d4c9871cda85a57127b2197f
Rafsalas19 <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-09-20 20:39:12 +0000

    Adding  pause in the job update ansible script to avoid overwriting job request and make fix to Nvidia job update function. Also change ansible and slurm scripts to allow for host files without tags

--- examples/slurm/prologMoneo.sh
+++ examples/slurm/prologMoneo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "[worker]" > /tmp/jobid_$SLURM_JOB_ID.txt
-echo $SLURM_JOB_NODELIST >> /tmp/jobid_$SLURM_JOB_ID.txt
+echo $SLURM_JOB_NODELIST > /tmp/jobid_$SLURM_JOB_ID.txt
 
 # The command below pipes stdout/stderr to files, This is necessary for ansible to behave correctly. 
 sudo -u <Your user> -H sh -c "cd  <Moneo Dir> &&  python3 moneo.py -j $SLURM_JOB_NAME.$SLURM_JOB_ID -c /tmp/jobid_$SLURM_JOB_ID.txt  > /tmp/moneolog.out 2>&1"
