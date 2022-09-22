aa84933bd6fc5309be870a2fcd0eb2fcb8692e16
Rafsalas19 <rafsalas@moneoVM.xonxbgpiawqu3papxwqrnhi42f.bx.internal.cloudapp.net>
2022-09-22 15:02:32 +0000

    Fixing lint issues.

--- moneo.py
+++ moneo.py
@@ -7,6 +7,7 @@ import os
 
 class MoneoCLI:
     '''Moneo CLI call'''
+
     def __init__(self, args):
         '''Init for MoneoCLI'''
         self.args = args
@@ -34,7 +35,10 @@ class MoneoCLI:
     def stop(self):
         '''Stops Moneo monitoring on hosts listed in the specified host ini file'''
         while True:
-            confirm = input("Are you sure you would like to perform a '" + self.args.type + "' shutdown of Moneo? (Y/n)\n")
+            confirm = input(
+                "Are you sure you would like to perform a '" +
+                self.args.type +
+                "' shutdown of Moneo? (Y/n)\n")
 
             if confirm.upper() == 'Y':
                 dep_cmd = 'ansible-playbook -i ' + self.args.host_ini + \
@@ -67,11 +71,14 @@ def check_deploy_shutdown(args, parser):
     deploy and shutdwon
     '''
     if (not os.path.isfile(args.host_ini)):
-        print(args.host_ini + " does not exist. Please provide a host file. i.e. host.ini.\n")
+        print(
+            args.host_ini +
+            " does not exist. Please provide a host file. i.e. host.ini.\n")
         parser.print_help()
         exit(1)
     if args.job_id:
-        print("Job Id cannot be specified during deployment and shutdown. Ignoring Job Id.\n")
+        print(
+            "Job Id cannot be specified during deployment and shutdown. Ignoring Job Id.\n")
     choices = ['manager', 'workers', 'full']
     if (args.type not in choices):
         print('Deployment/shutdown type not recognized or entered. Defaulted to the full option.\n')
@@ -92,13 +99,44 @@ if __name__ == '__main__':
         \nusage: %(prog)s [-j JOB_ID ] [-c HOST_INI] \
         \ni.e. python3 moneo.py -d -c ./host.ini full')
 
-    parser.add_argument('-c', '--host_ini', default='./host.ini', help='Provide filepath and name of ansible config file. The default is host.ini in the Moneo directory.')
-    parser.add_argument('-j', '--job_id', type=str, help='Job ID for filtering metrics by job group. Host.ini file required. Cannot be specified during deployment and shutdown')
-    parser.add_argument('-d', '--deploy', action='store_true', help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
-    parser.add_argument('-s', '--shutdown', action='store_true', help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
-    parser.add_argument('-i', '--insights', action='store_true', help='Experimental feature: Enable exporting of metrics to Azure Insights. Requires a valid instrumentation key and base_url for the Prometheus DB in config.ini')
-    parser.add_argument('type', metavar='type', type=str, default='full', nargs="?", help='Type of deployment/shutdown. Choices: {manager,workers,full}. Default: full.')
-    parser.add_argument('-p', '--profiler_metrics', action='store_true', default=False, help='Enable profile metrics (Tensor Core,FP16,FP32,FP64 activity). Addition of profile metrics encurs additional overhead on computer nodes.')
+    parser.add_argument(
+        '-c',
+        '--host_ini',
+        default='./host.ini',
+        help='Provide filepath and name of ansible config file. The default is host.ini in the Moneo directory.')
+    parser.add_argument(
+        '-j',
+        '--job_id',
+        type=str,
+        help='Job ID for filtering metrics by job group. Host.ini file required. Cannot be specified during deployment and shutdown')
+    parser.add_argument(
+        '-d',
+        '--deploy',
+        action='store_true',
+        help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument(
+        '-s',
+        '--shutdown',
+        action='store_true',
+        help='Requires config file to be specified (i.e. -c host.ini) or file to be in Moneo directory.')
+    parser.add_argument(
+        '-i',
+        '--insights',
+        action='store_true',
+        help='Experimental feature: Enable exporting of metrics to Azure Insights. Requires a valid instrumentation key and base_url for the Prometheus DB in config.ini')
+    parser.add_argument(
+        'type',
+        metavar='type',
+        type=str,
+        default='full',
+        nargs="?",
+        help='Type of deployment/shutdown. Choices: {manager,workers,full}. Default: full.')
+    parser.add_argument(
+        '-p',
+        '--profiler_metrics',
+        action='store_true',
+        default=False,
+        help='Enable profile metrics (Tensor Core,FP16,FP32,FP64 activity). Addition of profile metrics encurs additional overhead on computer nodes.')
     args = parser.parse_args()
 
     mCLI = MoneoCLI(args)
@@ -117,7 +155,9 @@ if __name__ == '__main__':
         mCLI.stop()
     elif args.job_id:
         if (not os.path.isfile(args.host_ini)):
-            print(args.host_ini + " does not exist. Please provide a host file. i.e. host.ini.\n")
+            print(
+                args.host_ini +
+                " does not exist. Please provide a host file. i.e. host.ini.\n")
             parser.print_help()
             exit(1)
         mCLI.jobID_update()
--- src/worker/helpers/shmem_helper.py
+++ src/worker/helpers/shmem_helper.py
@@ -2,6 +2,8 @@ from multiprocessing import shared_memory
 from multiprocessing import resource_tracker
 import sys
 
+"""Moneo Shared Memory Helper"""
+
 
 def clean_Leaked_shm(shm_name):
     ''' Clean up Resources'''
@@ -11,42 +13,50 @@ def clean_Leaked_shm(shm_name):
 
 
 class Shared_Mem_Mngr:
+    """Moneo Shared Memory Helper class"""
+
     def __init__(self, shm_name, isClient=False):
+        '''Init, set shm name and type of Access type Client/Server.'''
         self.isClient = isClient
         self.name = shm_name
-        self.shm = None
+        self.shmCreated = False
 
     def create_shm(self, data=None, size=1024):
+        '''Create memory of data size or of specified size. If data assign data.'''
         if self.isClient:  # client does not need to create shared mem just retrieve
             shm = self.get_shm()
         else:
-            if self.shm:
+            if self.shmCreated:
                 self.delete_shm()  # delete old shared memory
-            if data:
+            if data:  # If data provided creat shm of size data and assign
                 size = len(data.encode("utf8"))
-                shm = shared_memory.SharedMemory(create=True, size=size, name=self.name)
-                shm.buf[:size]=bytes(data,'UTF-8')
-            else:
-                shm = shared_memory.SharedMemory(create=True, size=size, name=self.name)
-        self.shm = shm
+                shm = shared_memory.SharedMemory(
+                    create=True, size=size, name=self.name)
+                shm.buf[:size] = bytes(data, 'UTF-8')
+            else:  # Create shm of specified size
+                shm = shared_memory.SharedMemory(
+                    create=True, size=size, name=self.name)
+        self.shmCreated = True
         return shm
 
     def delete_shm(self):
+        '''Delete shmem'''
         if self.isClient:  # the client should not be destroying memory
             return
         clean_Leaked_shm(self.name)
 
     def get_shm(self):
+        '''Return Shared Memory'''
         if self.isClient:
             try:
                 shm = shared_memory.SharedMemory(name=self.name)
-                resource_tracker.unregister(shm._name, "shared_memory")  # we need to tell the client process to not destroy memory
+                # we need to tell the client process to not destroy memory
+                resource_tracker.unregister(shm._name, "shared_memory")
                 return shm
-            except:
+            except BaseException:
                 return None
         else:
-            if not self.shm:
+            if not self.shmCreated:
                 return None
             else:
-                return self.shm
-    
+                return shared_memory.SharedMemory(name=self.name)
