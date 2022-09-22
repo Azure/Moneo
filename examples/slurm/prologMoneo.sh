a6a6b4733f0af0633dd4fa9978b4f82a10a8cec2
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2022-09-22 15:16:16 -0400

    Update prologMoneo.sh

--- examples/slurm/prologMoneo.sh
+++ examples/slurm/prologMoneo.sh
@@ -3,7 +3,7 @@
 echo $SLURM_JOB_NODELIST > /tmp/jobid_$SLURM_JOB_ID.txt
 
 # The command below pipes stdout/stderr to files, This is necessary for ansible to behave correctly. 
-sudo -u <Your user> -H sh -c "cd  <Moneo Dir> &&  python3 moneo.py -j $SLURM_JOB_NAME.$SLURM_JOB_ID -c /tmp/jobid_$SLURM_JOB_ID.txt  > /tmp/moneolog.out 2>&1"
+sudo -u <Your user> -H sh -c "cd  <Moneo Dir> &&  python3 moneo.py -j $SLURM_JOB_NAME.$SLURM_JOB_ID -c /tmp/jobid_$SLURM_JOB_ID.txt > /tmp/$SLURM_JOB_NAME.$SLURM_JOB_ID.Moneo.log 2>&1"
 
 exit 0
 
