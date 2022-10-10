0021175e076a2a05fafe2427a12e49c868f528de
yakovdyadkin <yakovdyad@gmail.com>
2022-10-10 18:13:47 +0000

    CPU profiling and formatting

--- src/worker/exporters/node_exporter.py
+++ src/worker/exporters/node_exporter.py
@@ -27,25 +27,29 @@ import psutil
 import prometheus_client
 
 FIELD_LIST = [
-    'net_rx', 
+    'net_rx',
     'net_tx',
-    'cpu_times_user', # use /proc/stat
-    'cpu_times_system', # use /proc/stat
-    'cpu_times_idle', # use /proc/stat
-    'cpu_times_iowait', # use /proc/stat
-    'cpu_util', # use /proc/stat
-    'cpu_frequency',
-    # 'mem_free', # use /proc/meminfo
-    'mem_available', # use /proc/meminfo
-    # 'mem_used', # use /proc/meminfo
-    'mem_util' # use /proc/meminfo
+    'cpu_util',  # use /proc/stat
+    'cpu_frequency',  # use /proc/cpuinfo
+    # 'mem_free',  # use /proc/meminfo
+    'mem_available',  # use /proc/meminfo
+    # 'mem_used',  # use /proc/meminfo
+    'mem_util'  # use /proc/meminfo
+]
+
+CPU_PROF_FIELDS = [
+    'cpu_times_user',  # use /proc/stat
+    'cpu_times_system',  # use /proc/stat
+    'cpu_times_idle',  # use /proc/stat
+    'cpu_times_iowait'  # use /proc/stat
 ]
 
 
 # feel free to copy and paste if os commands are needed
 def shell_cmd(args, timeout):
     """Helper Function for running subprocess"""
-    child = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    child = subprocess.Popen(args, stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
     try:
         result, errs = child.communicate(timeout=timeout)
     except subprocess.TimeoutExpired:
@@ -64,7 +68,8 @@ class NodeExporter(BaseExporter):
         super().__init__(node_fields, exp_config)
 
     def init_gauges(self):
-        '''Initialization of Prometheus parameters. Override in Child Class if needed'''      
+        '''Initialization of Prometheus parameters.
+           Override in Child Class if needed'''
         self.gauges = {}
         for field_name in self.node_fields:
             if 'cpu' in field_name:
@@ -75,16 +80,16 @@ class NodeExporter(BaseExporter):
                 )
             else:
                 self.gauges[field_name] = prometheus_client.Gauge(
-                'node_{}'.format(field_name),
-                'node_{}'.format(field_name),
-                ['job_id']
-            )
+                    'node_{}'.format(field_name),
+                    'node_{}'.format(field_name),
+                    ['job_id']
+                )
         print(self.gauges)
 
-
-    # example function of how to collect metrics from a command using the shell_cmd helper function
-    # the parent class will call this collect function to update the Prometheus gauges 
-    def collect(self, field_name): 
+    # example function of how to collect metrics from a command using the
+    # shell_cmd helper function the parent class will call this collect
+    # function to update the Prometheus gauges
+    def collect(self, field_name):
         '''Custom collection Method'''
         value = None
         if field_name == self.node_fields[0]:
@@ -104,28 +109,28 @@ class NodeExporter(BaseExporter):
             value = delta / (self.config['update_freq'])  # bandwidth
 
         elif 'cpu' in field_name:
-            value =  {}
-            if 'times' in field_name or 'util' in field_name:            
-                # If using psutil, times % and util % do not need to be calculated
-                # using the counters
+            value = {}
+            if 'times' in field_name or 'util' in field_name:
+                # If using psutil, times % and util % do not need to be
+                # calculated using the counters
 
                 if 'times' in field_name:
                     for id, time_percents in enumerate(psutil.cpu_times_percent(percpu=True)):
                         attribute = field_name.split('_')[-1]
-                        value['cpu' + str(id)] = getattr(time_percents,attribute)
-         
-                else: # CPU Util %
+                        value[str(id)] = getattr(time_percents, attribute)
+
+                else:  # CPU Util %
                     for id, util_percent in enumerate(psutil.cpu_percent(percpu=True)):
-                        value['cpu' + str(id)] = util_percent
+                        value[str(id)] = util_percent
 
             elif 'frequency' in field_name:
                 for id, freq in enumerate(psutil.cpu_freq(percpu=True)):
-                    value['cpu' + str(id)] = freq.current
+                    value[str(id)] = freq.current
 
         elif 'mem' in field_name:
             metric = field_name.split('_')[-1]
             virtual_mem = psutil.virtual_memory()
-            
+
             if metric == 'util':
                 value = getattr(virtual_mem, 'percent')
             else:
@@ -135,21 +140,14 @@ class NodeExporter(BaseExporter):
         else:
             value = 0
 
-        return value 
-
-    def process(self):
-        '''Perform metric collection'''
-        for field_name in self.node_fields:
-            value = self.collect(field_name)
-            self.handle_field(field_name, value)
+        return value
 
     def handle_field(self, field_name, value):
         '''Update metric value for gauge'''
         if 'cpu' in field_name:
-            #if 'times' in field_name or 'util' in field_name:
             logging.debug(f'Handeling field: {field_name}')
             for id, k in enumerate(value.keys()):
-                numa_domain='node' + str(id//config['numa_domain_size'])
+                numa_domain = str(id//config['numa_domain_size'])
                 logging.debug(f'Handeling key: {k}. Setting value: {value[k]}')
                 self.gauges[field_name].labels(
                     job_id=self.config['job_id'],
@@ -162,15 +160,17 @@ class NodeExporter(BaseExporter):
             ).set(value)
             logging.debug('Node exporter field %s: %s', field_name, str(value))
 
-    # This is called at the termination of the application. Can be used to close any open files.
+    # This is called at the termination of the application.
+    # Can be used to close any open files.
     def cleanup(self):
         '''Things that need to be called when signal to exit is given'''
         logging.info('Received exit signal, shutting down ...')
 
 
-# you will need to initialize your custom metric's file if we are exporting from a file
-# you may also want to initialize the config's counter member for the specific field
-def init_config(job_id, port=None):
+# you will need to initialize your custom metric's file if we are exporting
+# from a file you may also want to initialize the config's counter member
+# for the specific field
+def init_config(job_id, port=None, profiler_metrics=False):
     '''Example of config initialization'''
     global config
     if not port:
@@ -193,6 +193,10 @@ def init_config(job_id, port=None):
     domain_size = cores//numa_domains
     config['numa_domain_size'] = domain_size
 
+    # Add profiler metrics    
+    if profiler_metrics:
+        FIELD_LIST.extend(CPU_PROF_FIELDS)
+
     # initalize field specific config parameters
     for field_name in FIELD_LIST:
         if 'net' in field_name:
@@ -209,7 +213,7 @@ def init_config(job_id, port=None):
             if 'times' in field_name or 'util' in field_name:
                 config['fieldFiles'][field_name] = '/proc/stat'
 
-                # Call cpu_percent to get intial 0.0 values 
+                # Call cpu_percent to get intial 0.0 values
                 if 'util' in field_name:
                     _ = psutil.cpu_percent(percpu=True)
 
@@ -231,7 +235,8 @@ def init_signal_handler():
     signal.signal(signal.SIGTERM, exit_handler)
 
 
-# You can just copy paste this function. This is used to parse the log-level argument
+# You can just copy paste this function. This is used to parse the log-level
+# argument
 def get_log_level(args):
     '''Log level helper'''
     levelStr = args.log_level.upper()
@@ -264,11 +269,13 @@ def main():
                         execution in addition to warnings and errors DEBUG (4) \
                         - Log debugging information in addition to all')
     parser.add_argument("-p", "--port", type=int, default=None, help='Port to export metrics from')
+    parser.add_argument("-m", "--profiler_metrics", action="store_true", help="Enable profile metrics \
+                        (CPU User,System,Idle, IoWait times).")
     args = parser.parse_args()
 
     logging.basicConfig(level=get_log_level(args))
-    jobId = None #set a default job id of None
-    init_config(jobId, args.port)
+    jobId = None  # set a default job id of None
+    init_config(jobId, args.port, args.profiler_metrics)
     init_signal_handler()
 
     exporter = NodeExporter(FIELD_LIST, config)
