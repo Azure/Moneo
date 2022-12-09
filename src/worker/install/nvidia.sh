60654cb1a7843af2d4be202912e0f7c7063a9a43
rafsalas19 <rafsalas1987@hotmail.com>
2022-12-09 22:04:06 +0000

    Check to verify dcgm version

--- src/worker/install/nvidia.sh
+++ src/worker/install/nvidia.sh
@@ -48,19 +48,40 @@ alma_dcgm_install () {
 		rm -f datacenter-gpu-manager-${DCGM_VERSION}-1-x86_64.rpm
 }
 
+check_min_dcgm_ver(){
+	DCGM_VER=`dcgmi --version |grep version | awk -F ': ' '{print $2}'`
+	REQ_VER="2.4.4"
+	if [ "$(printf '%s\n' "$REQ_VER" "$DCGM_VER" | sort -V | head -n1)" = "$REQ_VER" ]; then 
+			echo "A suitable version of Dcgm is already installed"
+	else
+			echo "removing old DCGM"
+			# remove old version
+			if [[ $distro =~ "Ubuntu" ]]; then
+				nv-hostengine -t > /dev/null 2>&1
+				apt -y remove datacenter-gpu-manager
+			elif [[ $distro =~ "AlmaLinux" ]]; then
+				nv-hostengine -t
+				yum -y remove datacenter-gpu-manager
+			fi
+			$1
+	fi
+
+}
+
 
 # install DCGM
 if [[ $distro =~ "Ubuntu" ]]; then
 	dcgm_check=`sudo dpkg-query -l`
 	if [[ $dcgm_check =~ "datacenter-gpu-manager" ]]; then
-		echo "Dcgm already installed"
+		check_min_dcgm_ver ubuntu_dcgm_install
+
 	else
 		ubuntu_dcgm_install
 	fi
 elif [[ $distro =~ "AlmaLinux" ]]; then
 	dcgm_check=`rpm -qa`
 	if [[ $dcgm_check =~ "datacenter-gpu-manager" ]]; then
-		echo "Dcgm already installed"
+		check_min_dcgm_ver alma_dcgm_install
 	else
 		alma_dcgm_install
 	fi
