266bd5d072e22a2962a8465dcc0b010562bc5642
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-03-22 11:15:28 -0400

    Removing starting the service

--- linux_service/configure_service.sh
+++ linux_service/configure_service.sh
@@ -17,7 +17,3 @@ systemctl daemon-reload
 systemctl enable moneo@node_exporter.service
 systemctl enable moneo@net_exporter.service
 systemctl enable moneo@nvidia_exporter.service
-
-systemctl start moneo@node_exporter.service
-systemctl start moneo@net_exporter.service
-systemctl start moneo@nvidia_exporter.service
