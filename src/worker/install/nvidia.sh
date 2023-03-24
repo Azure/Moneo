c2c98c648660dcad5e913717444eb71fff76225d
Ubuntu <rafsalas@publisher-testvm.xa3bd5c0a51efkihrk0o3dy2mc.bx.internal.cloudapp.net>
2023-03-24 19:46:39 +0000

    fixing install for ubuntu

--- src/worker/install/nvidia.sh
+++ src/worker/install/nvidia.sh
@@ -17,13 +17,26 @@ ubuntu_dcgm_install () {
 
 alma_dcgm_install () {
 		echo "Installing Dcgm"
-		yum-config-manager --add-repo=https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
-		sudo yum install dcgm
+		DCGM_VERSION=2.4.4
+		DCGM_URL=https://azhpcstor.blob.core.windows.net/azhpc-images-store/datacenter-gpu-manager-${DCGM_VERSION}-1-x86_64.rpm
+		wget --retry-connrefused --tries=3 --waitretry=5 $DCGM_URL
+		FILE_NAME=$(basename $DCGM_URL)
+		RLINK=$(readlink -f $FILE_NAME)
+		Check="1d8fbe97797fada8048a7832bfac4bc7d3ad661bb24163d21324965ae7e7817d"
+		checksum=`sha256sum $RLINK | awk '{print $1}'`
+		if [[ $checksum != $Check ]]
+		then
+			echo "*** Error - Checksum verification failed"
+			echo "*** Error - Checksum verification failed" > dcgm_fail.log
+			exit -1
+		fi
+		rpm -i datacenter-gpu-manager-${DCGM_VERSION}-1-x86_64.rpm
+		rm -f datacenter-gpu-manager-${DCGM_VERSION}-1-x86_64.rpm
 }
 
 check_min_dcgm_ver(){
 	DCGM_VER=`dcgmi --version |grep version | awk -F ': ' '{print $2}'`
-	REQ_VER="3.1.6"
+	REQ_VER=$2
 	if [ "$(printf '%s\n' "$REQ_VER" "$DCGM_VER" | sort -V | head -n1)" = "$REQ_VER" ]; then 
 			echo "A suitable version of Dcgm is already installed"
 	else
@@ -44,7 +57,7 @@ check_min_dcgm_ver(){
 if [[ $distro =~ "Ubuntu" ]]; then
 	dcgm_check=`sudo dpkg-query -l`
 	if [[ $dcgm_check =~ "datacenter-gpu-manager" ]]; then
-		check_min_dcgm_ver ubuntu_dcgm_install
+		check_min_dcgm_ver ubuntu_dcgm_install "3.1.6"
 
 	else
 		ubuntu_dcgm_install
@@ -52,7 +65,7 @@ if [[ $distro =~ "Ubuntu" ]]; then
 elif [[ $distro =~ "AlmaLinux" ]]; then
 	dcgm_check=`rpm -qa`
 	if [[ $dcgm_check =~ "datacenter-gpu-manager" ]]; then
-		check_min_dcgm_ver alma_dcgm_install
+		check_min_dcgm_ver alma_dcgm_install "2.4.4"
 	else
 		alma_dcgm_install
 	fi
