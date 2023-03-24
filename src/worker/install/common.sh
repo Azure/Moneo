01d6b5fb3a1a5c69d78256f607507966fd043bb6
Ubuntu <rafsalas@publisher-testvm.xa3bd5c0a51efkihrk0o3dy2mc.bx.internal.cloudapp.net>
2023-03-24 18:04:10 +0000

    adding python3-dev to install

--- src/worker/install/common.sh
+++ src/worker/install/common.sh
@@ -3,5 +3,14 @@
 set -e
 
 # install dependencies
+# install DCGM
+if [[ $distro =~ "Ubuntu" ]]; then
+    apt-get install -y python3-dev
+elif [[ $distro =~ "AlmaLinux" ]]; then
+    yum install -y python3-dev
+else
+	echo "OS version is not supported"
+fi
+
 command -v pip3 >/dev/null 2>&1 || python3 <(curl -s https://bootstrap.pypa.io/get-pip.py)
 python3 -m pip install prometheus_client psutil filelock opentelemetry-sdk opentelemetry-exporter-otlp
