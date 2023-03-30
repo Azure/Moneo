b8fbd9b82b47c7e1a8814b69105ce129db33962c
Ubuntu <rafsalas@ub20n8855000008.orxxfqth034upnhnpwma2s5jtc.ax.internal.cloudapp.net>
2023-03-30 18:01:56 +0000

    integrating geneva publisher into linux services

--- linux_service/stop_moneo_services.sh
+++ linux_service/stop_moneo_services.sh
@@ -4,3 +4,9 @@ systemctl stop moneo@node_exporter.service
 systemctl stop moneo@net_exporter.service
 systemctl stop moneo@nvidia_exporter.service
 systemctl stop moneo_publisher.service
+
+if [[ $(docker ps -a | grep genevamdmagent) ]]; then
+    echo "Stopping Geneva Metrics Extension(MA) container"
+    docker stop genevamdmagent
+    docker rm genevamdmagent
+fi
