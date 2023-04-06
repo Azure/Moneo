7d0c2f59d41f1c12c8743ee96e04bc5aca6c4bc7
Ubuntu <rafsalas@ub20n8855000001.orxxfqth034upnhnpwma2s5jtc.ax.internal.cloudapp.net>
2023-04-06 20:19:12 +0000

    slight change to make thins uniform

--- linux_service/start_moneo_services.sh
+++ linux_service/start_moneo_services.sh
@@ -5,7 +5,6 @@ MONEO_PATH=$2
 systemctl enable moneo@node_exporter.service
 systemctl enable moneo@net_exporter.service
 systemctl enable moneo@nvidia_exporter.service
-systemctl enable moneo_publisher.service
 
 systemctl start moneo@node_exporter.service
 systemctl start moneo@net_exporter.service
@@ -18,10 +17,12 @@ then
         #starts Geneva agent
         $MONEO_PATH/src/worker/start_geneva.sh cert $MONEO_PATH/src/worker/publisher/config
         sleep 5 # wait a bit for the exporters to start
+        systemctl enable moneo_publisher.service
         systemctl start moneo_publisher.service 
     elif [ "$PUBLISHER" = "azure_monitor" ];
     then
         sleep 5 # wait a bit for the exporters to start
+        systemctl enable moneo_publisher.service
         systemctl start moneo_publisher.service 
     else
         echo "Either PUBLISHER OR MONEO_PATH unrecognized. PUBLISHER can be geneva or azure_monitor. If publisher is geneva MONEO_PATH must be defined."
