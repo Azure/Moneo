ce04b64bf8878b57853fa9fefb052790e6f83b87
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-04-25 10:44:06 -0400

    Node exp bug fix part 2 (#53)
    * bug fix to make node_exporter more robust  in wrt regex errors
    
    * Change curl to run in silent mode and gather data
    
    * another bug fix for node exporter to add more robustness
    
    * fix lint issue
    
    ---------
    
    Co-authored-by: Ubuntu <rafsalas@ub22h3e8f000003.dqjt2vfkzmou3mpiz4ibgqqi1c.ax.internal.cloudapp.net>
    Co-authored-by: Ubuntu <azhpcuser@headnode.mle2bajryc3utk4erkvtjmazef.jx.internal.cloudapp.net>

--- src/worker/exporters/node_exporter.py
+++ src/worker/exporters/node_exporter.py
@@ -132,27 +132,31 @@ class NodeExporter(BaseExporter):
                 # psutil returns virtual memory stats in bytes, convert to kB
                 value = getattr(virtual_mem, metric) / 1024
         elif 'xid' in field_name or "link_flap" in field_name:
-            value = {}
-            cmd = config['command'][field_name]
-            # check if error present in logs
-            field_check = shell_cmd(cmd, 5)
-            # strip empty lines
-            result = [line for line in field_check.split(
-                '\n') if line.strip() != '']
-            for line in result:
-                timestamp = re.search(
-                    r"\w\w\w\s+\d+\s\d\d:\d\d:\d\d", line).group()
-                if 'xid' in field_name:
-                    results = re.search(r"\(.+\):\s\d\d", line).group().split()
-                    pci = results[0].replace(
-                        '(PCI:', '').replace('):', '')[-10:]
-                    value[pci] = {timestamp: int(results[1])}
-                else:  # link flap
-                    results = re.search(r"\bib\d:", line)
-                    if not results:
-                        continue
-                    hca = results.group().replace(':', '')
-                    value[hca] = {timestamp: 1}
+            try:
+                value = {}
+                cmd = config['command'][field_name]
+                # check if error present in logs
+                field_check = shell_cmd(cmd, 5)
+                # strip empty lines
+                result = [line for line in field_check.split(
+                    '\n') if line.strip() != '']
+                for line in result:
+                    timestamp = re.search(
+                        r"\w\w\w\s+\d+\s\d\d:\d\d:\d\d", line).group()
+                    if 'xid' in field_name:
+                        results = re.search(r"\(.+\):\s\d\d", line).group().split()
+                        pci = results[0].replace(
+                            '(PCI:', '').replace('):', '')[-10:]
+                        value[pci] = {timestamp: int(results[1])}
+                    else:  # link flap
+                        results = re.search(r"\bib\d:", line)
+                        if not results:
+                            continue
+                        hca = results.group().replace(':', '')
+                        value[hca] = {timestamp: 1}
+            except Exception as e:
+                logging.error('Raised exception. Message: %s', e)
+                pass
         else:
             value = 0
         return value
