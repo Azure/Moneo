184bee5399c4acd1ee78035ee8e519666fe832e0
Ubuntu <rafsalas@a100vm.dnkq5svzo1wedbjjy0q5ykz5bb.bx.internal.cloudapp.net>
2023-08-04 21:54:25 +0000

    adding scripts to deploy managed infra

---
+++ deploy_managed_infra/deploy_moneo_infrastructure.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if az account show &>/dev/null; then
+  # You are logged in
+  echo "You are logged in to the Azure CLI."
+  # Add other commands you want to run when logged in
+else
+  # You are not logged in
+  echo "You are not logged in to the Azure CLI. Please run 'az login' to log in."
+  # Add other commands or actions you want to take when not logged in
+  exit 1
+fi
+
+# Change the namimg if desired
+prometheus_mon_workspace="moneo-amw"
+rgroup="moneo-rg" 
+idname="moneo-identity"
+grafana_name="moneo-dashboard"
+
+# change location and subscription
+location="southcentralus"
+subid="<sub-id>"
+
+az account set --subscription $subid
+
+az group create --name $rgroup --location $location
+
+az deployment group create \
+  --name moneoInfraDeploy \
+  --resource-group $rgroup \
+  --template-file managed_infra_template.json \
+  --parameters  accounts_moneo_amw_name=$prometheus_mon_workspace \
+  --parameters  grafana_moneo_grafana_name=$grafana_name \
+  --parameters userAssignedIdentities_moneo_identity_name=$idname \
+  --parameters moeno_location=$location \
+  --verbose
+
+if [ $? -eq 0 ]; then
+    echo "The last command completed successfully."
+else
+    echo "The last command encountered an error."
+    exit 1
+fi
+
+sleep 60s
+
+# Get the Object ID of the Managed Identity
+managedIdentityObjectId=$(az identity show -n $idname -g $rgroup --subscription $subid --query principalId -o tsv)
+dataCollectionEndpointResourceId=$(az monitor account show  -n $prometheus_mon_workspace -g  $rgroup  --subscription $subid --query "defaultIngestionSettings.dataCollectionEndpointResourceId")
+dataCollectionRuleResourceId=$(az monitor account show  -n $prometheus_mon_workspace -g  $rgroup  --subscription $subid --query "defaultIngestionSettings.dataCollectionRuleResourceId")
+dataCollectionEndpointResourceId=$(echo "$dataCollectionEndpointResourceId" | sed 's/"//g')
+dataCollectionRuleResourceId=$(echo "$dataCollectionRuleResourceId" | sed 's/"//g')
+prom_work_space="/subscriptions/$subid/resourceGroups/$rgroup/providers/microsoft.monitor/accounts/$prometheus_mon_workspace"
+
+# Assign "Monitoring Metrics Publisher" role to the Managed Identity for Data Collection Endpoint
+az role assignment create --role "Monitoring Metrics Publisher" --assignee-object-id $managedIdentityObjectId --scope $dataCollectionEndpointResourceId --subscription $subid
+
+# Assign "Monitoring Metrics Publisher" role to the Managed Identity for Data Collection Rule
+az role assignment create --role "Monitoring Metrics Publisher" --assignee-object-id $managedIdentityObjectId --scope $dataCollectionRuleResourceId --subscription $subid
+
+# Assign "Monitoring Metrics Publisher" role to the Managed Identity for Azure monitor workspace
+az role assignment create --role "Monitoring Metrics Publisher" --assignee-object-id $managedIdentityObjectId --scope $prom_work_space
