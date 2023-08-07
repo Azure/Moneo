e881303b3ea1cba2796666026195a8e051046d44
Yang Wang <yangwang1@microsoft.com>
2023-08-07 07:43:50 +0000

    deprecate prom sidecar

--- src/worker/start_managed_prometheus.sh
+++ src/worker/start_managed_prometheus.sh
@@ -4,9 +4,7 @@ INSTANCE_NAME=$(hostname)
 WORK_DIR=/tmp/moneo-worker
 PROM_CONFIG=$WORK_DIR/prometheus.yml
 CONFIG_DIR=$WORK_DIR/publisher/config
-PROM_SIDECAR_CONFIG=$CONFIG_DIR/prom_sidecar_config.json
-SIDECAR_VERSION='prom-remotewrite-20230323.1'
-SIDERCAR_DOCKER='mcr.microsoft.com/azuremonitor/prometheus/promdev/prom-remotewrite:'$SIDECAR_VERSION
+MANAGED_PROM_CONFIG=$CONFIG_DIR/managed_prom_config.json
 
 get_subscription(){
     subscription_name=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute/subscriptionId?api-version=2021-02-01&format=text")
@@ -34,6 +32,9 @@ SUBSCRIPTION_NAME=$(get_subscription)
 CLUSTER_NAME=$(get_cluster_name)
 PHYS_HOST_NAME=$(get_physical_host_name)
 
+IDENTITY_CLIENT_ID=$(jq -r '.IDENTITY_CLIENT_ID' $MANAGED_PROM_CONFIG)
+INGESTION_ENDPOINT=$(jq -r '.INGESTION_ENDPOINT' $MANAGED_PROM_CONFIG)
+
 generate_prom(){
     DCMG_TARGET="        - $INSTANCE_NAME:8000\n"
     NET_TARRGET="        - $INSTANCE_NAME:8001\n"
@@ -44,6 +45,8 @@ generate_prom(){
     sed -i -r "s/cluster_name/$CLUSTER_NAME/" $PROM_CONFIG
     sed -i -r "s/instance_name/$INSTANCE_NAME/" $PROM_CONFIG
     sed -i -r "s/physical_host_name/$PHYS_HOST_NAME/" $PROM_CONFIG
+    sed -i -r "s@ingestion_endpoint@$INGESTION_ENDPOINT@" $PROM_CONFIG
+    sed -i -r "s/identity_client_id/$IDENTITY_CLIENT_ID/" $PROM_CONFIG
 
     sed -i -r "s/\s+- moneo-worker-0:8000/$DCMG_TARGET/" $PROM_CONFIG
     sed -i -r "s/\s+- moneo-worker-0:8001/$NET_TARRGET/" $PROM_CONFIG
@@ -65,27 +68,10 @@ docker run --name prometheus \
     --config.file=/etc/prometheus/prometheus.yml \
     --web.enable-admin-api
 
-# get prom sidecar config
-IDENTITY_CLIENT_ID=$(jq -r '.IDENTITY_CLIENT_ID' $PROM_SIDECAR_CONFIG)
-INGESTION_ENDPOINT=$(jq -r '.INGESTION_ENDPOINT' $PROM_SIDECAR_CONFIG)
-
 echo "SUBSCRIPTION_NAME: $SUBSCRIPTION_NAME"
 echo "CLUSTER_NAME: $CLUSTER_NAME"
 echo "INSTANCE_NAME: $INSTANCE_NAME"
+echo "PHYS_HOST_NAME: $PHYS_HOST_NAME"
 
 echo "IDENTITY_CLIENT_ID: $IDENTITY_CLIENT_ID"
 echo "INGESTION_ENDPOINT: $INGESTION_ENDPOINT"
-echo "SIDERCAR_DOCKER: $SIDERCAR_DOCKER"
-
-# pull prom sidecar image
-docker pull $SIDERCAR_DOCKER
-docker tag  $SIDERCAR_DOCKER prometheus_sidecar
-docker rmi  $SIDERCAR_DOCKER
-
-# start sidecar container
-docker rm -f prometheus_sidecar || true
-sudo docker run --name=prometheus_sidecar \
-    -it --net=host --uts=host -d \
-    -e CLUSTER=$CLUSTER_NAME -e LISTENING_PORT=8081 \
-    -e IDENTITY_TYPE=userAssigned -e AZURE_CLIENT_ID=$IDENTITY_CLIENT_ID \
-    -e INGESTION_URL=$INGESTION_ENDPOINT prometheus_sidecar
