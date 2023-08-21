8ef6472fa71f28fb594a6ff2b71c23af218ccc84
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-08-21 11:05:38 -0400

    slurm integartion update (#65)
    * slurm integartion update
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Ubuntu <hpcuser@slurm-headnode.kc2qzvt3q20evnd3dnumikf5vg.jx.internal.cloudapp.net>

--- examples/slurm/epilogMoneo.sh
+++ examples/slurm/epilogMoneo.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
+log_file=/tmp/moneo-worker/id_change.log
 
-echo $SLURM_JOB_ID > /tmp/testfile.txt
-
-# The command below pipes stdout/stderr to files, Moneo will still update the respective moneo logs
-sudo -u <Your user> -H sh -c "cd  <Moneo DIR> &&  python3 moneo.py -j None -c /tmp/jobid_$SLURM_JOB_ID.txt >> /tmp/$SLURM_JOB_NAME.$SLURM_JOB_ID.Moneo.log 2>&1"
-
-rm /tmp/jobid_$SLURM_JOB_ID.txt
+sudo /tmp/moneo-worker/jobIdUpdate.sh  "None" > "$log_file" 2>&1
 
 exit 0
-
-
