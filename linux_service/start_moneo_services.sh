d2a9ef44bd09076c6e294901c5df347955299e82
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-09-07 12:27:47 -0400

    redirecting output messages w/ little value and modifying start scrip… (#66)
    redirecting output messages w/ little value and modifying start scripts to make /opt/azurehpc directory the default dir for moneo
    
    Co-authored-by: Ubuntu <azhpcuser@iad21-1709.uoymqg2vopruth505qffmafr1e.bx.internal.cloudapp.net>

--- linux_service/start_moneo_services.sh
+++ linux_service/start_moneo_services.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-WITH_PUBLISHER=$3
-WITH_MANAGED_PROM=$2
-MONEO_PATH=$1
+WITH_AZ_MON=$1
+
+# Two modes of operation managed prometheus (default) and  az monitor (WITH_AZ_MON) enabled w/ WITH_AZ_MON=true
+
+# User can modify this if they need to change
+MONEO_PATH=/opt/azurehpc/tools/Moneo
+
 
 if [[ ! -d "$MONEO_PATH" ]];
 then
@@ -10,19 +14,19 @@ then
 fi
 
 
-
 procs=("net_exporter" "node_exporter")
 
 if lspci | grep -iq NVIDIA ; then
     procs+=("nvidia_exporter")
 fi
 
-if [[ -n $WITH_PUBLISHER && $WITH_PUBLISHER = true ]]; then
+if [[ -n $WITH_AZ_MON && $WITH_AZ_MON = true ]]; then
     procs+=("metrics_publisher")
 fi
 
 function proc_check(){
     CHECK=`ps -eaf | grep /tmp/moneo-worker/`
+    WITH_MANAGED_PROM=$1
     for substring in "${procs[@]}"; do
         if [[ $CHECK == *"$substring"* ]]; then
             echo "$substring service started as expected."
@@ -31,12 +35,13 @@ function proc_check(){
             exit 1
         fi
     done
+
     if [[ -n $WITH_MANAGED_PROM && $WITH_MANAGED_PROM = true ]];
     then
         if [[ $(docker ps -a | grep prometheus) ]] ; then
             echo "Prometheus docker containers running."
         else
-            echo "Prometheus failed to start. Please ensure you have the proper user managed identity assigned to your VMSS/VM. (moneo-umi)"
+            echo "Prometheus failed to start. Please ensure you have the proper user managed identity assigned to your VMSS/VM."
             exit 1
         fi
     fi
@@ -44,8 +49,7 @@ function proc_check(){
     exit 0
 }
 
-$MONEO_PATH/linux_service/moneo_prestart.sh $MONEO_PATH
-
+$MONEO_PATH/linux_service/moneo_prestart.sh $MONEO_PATH 2> /dev/null
 
 systemctl enable moneo@node_exporter.service
 systemctl enable moneo@net_exporter.service
@@ -55,14 +59,13 @@ systemctl start moneo@node_exporter.service
 systemctl start moneo@net_exporter.service
 systemctl start moneo@nvidia_exporter.service
 
-if [[ -n $WITH_MANAGED_PROM && $WITH_MANAGED_PROM = true ]]; then
-    $MONEO_PATH/src/worker/start_managed_prometheus.sh
-fi
-
-if [[ -n $WITH_PUBLISHER && $WITH_PUBLISHER = true ]]; then
+if [[ -n $WITH_AZ_MON && $WITH_AZ_MON = true ]]; then
     sleep 5 # wait a bit for the exporters to start
     systemctl enable moneo_publisher.service
     systemctl start moneo_publisher.service 
+    proc_check false
+else
+    $MONEO_PATH/src/worker/start_managed_prometheus.sh 2> /dev/null
+    sleep 5
+    proc_check true
 fi
-
-proc_check
