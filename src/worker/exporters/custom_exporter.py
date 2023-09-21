ec5f5e9fd9416cea6fe19fa3bda48915bbfe3121
Yang Wang <yangwang1@microsoft.com>
2023-09-21 05:54:33 +0000

    udpate custom_exporter

--- src/worker/exporters/custom_exporter.py
+++ src/worker/exporters/custom_exporter.py
@@ -14,24 +14,22 @@ FIELD_LIST = []
 metrics_list = []
 GPU_Mapping = {}
 IB_Mapping = {}
-# feel free to copy and paste if os commands are needed
 
 def shell_cmd(cmd, timeout):
     """Helper Function for running subprocess"""
     args = shlex.split(cmd)
-    child = subprocess.Popen(args, stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
     try:
-        result, errs = child.communicate(timeout=timeout)
+        result = subprocess.check_output(args, stderr=subprocess.STDOUT, timeout=timeout)
+        return result.decode()
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Command {' '.join(args)} failed with error: {e}")
+        return 'Error'
     except subprocess.TimeoutExpired:
-        child.kill()
-        print("Command " + " ".join(args) + ", Failed on timeout")
-        result = 'TimeOut'
-        return result
-    return result.decode()
+        logging.error(f"Command {' '.join(args)} timed out")
+        return 'Timeout'
 
 def init_ib_config():
-    # IB mapping
+    """Initialize InfiniBand configuration"""
     cmd = 'ibv_devinfo -l'
     result = shell_cmd(cmd, 5)
     if 'HCAs found' in result or 'HCA found' in result:
@@ -40,21 +38,18 @@ def init_ib_config():
             for idx, ib in enumerate(result):
                 if "ib" not in ib:
                     continue
-                if len(ib):
+                if ib:
                     mapping = re.search(r"ib\d", ib.strip()).group()
                     IB_Mapping[mapping] = ib.strip() + ':1'
-                    config['ib_port'][str(idx)] = ib.strip() + ':1'
+                    config['ib_port'][str(idx)] = IB_Mapping[mapping]
         except Exception as e:
-            print(e)
-            pass
-
+            logging.error(f"Error parsing IB info: {e}")
 
 def init_nvidia_config():
+    """Initialize NVIDIA configuration"""
     global config
     global GPU_Mapping
-    global IB_Mapping
-    global FIELD_LIST
-    # check if nvidiaVM
+    # Check if it's an NVIDIA VM
     nvArch = os.path.exists('/dev/nvidiactl')
     if nvArch:
         cmd = 'nvidia-smi -L'
@@ -62,68 +57,65 @@ def init_nvidia_config():
         gpuCount = len(result.split('\nGPU'))
         try:
             for gpu in range(gpuCount):
-                cmd = 'nvidia-smi -q -g ' + str(gpu) + ' -d ACCOUNTING'
+                cmd = f'nvidia-smi -q -g {gpu} -d ACCOUNTING'
                 result = shell_cmd(cmd, 5)
                 pci = re.search(r"\w+:\w\w:\w\w\.", result).group().lower()
                 pci = pci.replace('.', '')[-10:]
-                GPU_Mapping[pci] = str(gpu)  # pci mapping
-                config['gpu_id'][str(gpu)] = str(gpu)
+                GPU_Mapping[pci] = str(gpu)  # PCI mapping
+                config['gpu_id'][str(gpu)] = GPU_Mapping[pci]
         except Exception as e:
-            print(e)
-            pass
+            logging.error(f"Error initializing NVIDIA config: {e}")
 
-
-class CustomExporter():
+class CustomExporter:
     '''Example custom node exporter'''
 
     def __init__(self, custom_metrics_file_path):
-        '''initializes parent class'''
+        '''Initializes the exporter'''
         self.custom_metrics_file_path = custom_metrics_file_path
         self.init_connection()
         self.init_gauges()
         signal.signal(signal.SIGUSR1, self.jobID_update_flag)
 
     def init_connection(self):
+        """Initialize Prometheus connection"""
         prometheus_client.start_http_server(config['listen_port'])
-        logging.info('Started prometheus client')
-
+        logging.info('Started Prometheus client')
         self.field_list = [] 
         self.field_list.extend(FIELD_LIST)
-        logging.info('Publishing fields: {}'.format(','.join(self.field_list)))
+        logging.info('Publishing fields: %s', ','.join(self.field_list))
 
     def init_gauges(self):
-        '''Initialization of Prometheus parameters.
-        Override in Child Class if needed'''
+        '''Initialization of Prometheus parameters'''
         self.gauges = {}
         for field_name in self.field_list:
             metric_name = ''
             if "gpu_" in field_name:
-                metric_name = field_name.split('_')[0] + '_' + field_name.split('_')[-1]
+                metric_name = f"{field_name.split('_')[0]}_{field_name.split('_')[2]}"
                 if metric_name in metrics_list:
                     continue
                 self.gauges[metric_name] = prometheus_client.Gauge(
-                    'custom_{}'.format(metric_name),
-                    'custom_{}'.format(metric_name),
+                    f'custom_{metric_name}',
+                    f'custom_{metric_name}',
                     ['gpu_id', 'job_id']
                 )
             elif "ib_" in field_name:
-                metric_name = field_name.split('_')[0] + '_' + field_name.split('_')[2]
+                metric_name = f"{field_name.split('_')[0]}_{field_name.split('_')[2]}"
                 if metric_name in metrics_list:
                     continue
                 self.gauges[metric_name] = prometheus_client.Gauge(
-                    'custom_{}'.format(metric_name),
-                    'custom_{}'.format(metric_name),
+                    f'custom_{metric_name}',
+                    f'custom_{metric_name}',
                     ['ib_port', 'job_id']
                 ) 
             else:
                 self.gauges[field_name] = prometheus_client.Gauge(
-                    'custom_{}'.format(field_name),
-                    'custom_{}'.format(field_name),
+                    f'custom_{field_name}',
+                    f'custom_{field_name}',
                     ['job_id']
                 )
             if metric_name != '' and metric_name not in metrics_list:
                 metrics_list.append(metric_name)
-                logging.info('Publishing metric: {}'.format(metric_name))
+                logging.info('Publishing metric: %s', metric_name)
 
     def collect_custom_metrics(self):
         '''Updates custom metrics'''
@@ -137,15 +129,14 @@ class CustomExporter():
     def process(self):
         custom_metrics = self.collect_custom_metrics()
         for node_field in self.field_list:
-            value = custom_metrics[node_field] if node_field in custom_metrics.keys() else None
-            if not value:
-                continue
-            self.handle_field(node_field, value)
+            value = custom_metrics.get(node_field)
+            if value is not None:
+                self.handle_field(node_field, value)
 
     def update_gauges_field(self, field_name, value, config):
         if "gpu_" in field_name:
             gpu_idx = field_name.split('_')[1]
-            metric_name = field_name.split('_')[0] + '_' + field_name.split('_')[2]
+            metric_name = f"{field_name.split('_')[0]}_{field_name.split('_')[2]}"
             if int(gpu_idx) < len(GPU_Mapping):
                 self.gauges[metric_name].labels(
                     config['gpu_id'][gpu_idx],
@@ -153,7 +144,7 @@ class CustomExporter():
                 ).set(value)
         elif "ib_" in field_name:
             ib_port = field_name.split('_')[1]
-            metric_name = field_name.split('_')[0] + '_' + field_name.split('_')[2]
+            metric_name = f"{field_name.split('_')[0]}_{field_name.split('_')[2]}"
             if int(ib_port) < len(IB_Mapping):
                 self.gauges[metric_name].labels(
                     config['ib_port'][ib_port],
@@ -164,21 +155,21 @@ class CustomExporter():
                 config['job_id']
             ).set(value)
 
-    def handle_field(self, field_name, value):  # noqa: C901
+    def handle_field(self, field_name, value):
         '''Update metric value for gauge'''
         self.update_gauges_field(field_name, value, config)
         logging.debug('Custom exporter field %s: %s', field_name, str(value))
 
     def remove_metric(self, field_name):
-        """Remove mertics label values"""
+        """Remove metrics label values"""
         if "gpu_" in field_name:
             gpu_idx = field_name.split('_')[1]
-            metric_name = field_name.split('_')[0] + field_name.split('_')[2]
+            metric_name = f"{field_name.split('_')[0]}{field_name.split('_')[2]}"
             if int(gpu_idx) < len(GPU_Mapping):
                 self.gauges[metric_name].remove(config['gpu_id'][gpu_idx], config['job_id'])
         elif "ib_" in field_name:
             ib_port = field_name.split('_')[1]
-            metric_name = field_name.split('_')[0] + field_name.split('_')[2]
+            metric_name = f"{field_name.split('_')[0]}{field_name.split('_')[2]}"
             if int(ib_port) < len(IB_Mapping):
                 ib_port = field_name.split('_')[1]
             self.gauges[metric_name].remove(config['ib_port'][ib_port], config['job_id'])
@@ -186,7 +177,7 @@ class CustomExporter():
             self.gauges[field_name].remove(config['job_id'])
 
     def jobID_update_flag(self, signum, stack):
-        '''Sets job update flag when user defined signal comes in'''
+        '''Sets job update flag when user-defined signal comes in'''
         global job_update
         job_update = True
 
@@ -195,16 +186,13 @@ class CustomExporter():
         # Remove last set of label values
         for field_name in self.field_list:
             self.remove_metric(field_name)
-
         # Update job id
         with open('/tmp/moneo-worker/curr_jobID') as f:
             config['job_id'] = f.readline().strip()
         logging.debug('Job ID updated to %s', config['job_id'])
 
-    # This is called at the termination of the application.
-    # Can be used to close any open files.
     def cleanup(self):
-        '''Things that need to be called when signal to exit is given'''
+        '''Things that need to be called when a signal to exit is given'''
         logging.info('Received exit signal, shutting down ...')
 
     def loop(self):
@@ -212,19 +200,16 @@ class CustomExporter():
         job_update = False
         try:
             while True:
-                if (job_update):
+                if job_update:
                     self.jobID_update()
                 self.process()
                 time.sleep(config['update_freq'])
-                if config['exit'] is True:
+                if config['exit']:
                     logging.info('Received exit signal, shutting down ...')
                     break
         except KeyboardInterrupt:
             pass
 
-# you will need to initialize your custom metric's file if we are exporting
-# from a file you may also want to initialize the config's counter member
-# for the specific field
 def init_config(job_id, port=None):
     '''Example of config initialization'''
     global config
@@ -240,21 +225,19 @@ def init_config(job_id, port=None):
         'job_id': job_id,
     }
 
-def init_custom_metrics(custom_metcis_file_path):
+def init_custom_metrics(custom_metrics_file_path):
     '''Example of custom metrics initialization'''
-    metrics_json_path = custom_metcis_file_path
+    metrics_json_path = custom_metrics_file_path
     if not os.path.exists(metrics_json_path):
         logging.error('Custom metrics file does not exist. Exiting...')
         sys.exit(1)
     with open(metrics_json_path) as f:
         custom_metrics = json.load(f)
 
-    for metrics_name, _ in custom_metrics.items():
-        FIELD_LIST.append(metrics_name)
+    FIELD_LIST.extend(custom_metrics.keys())
 
-# You can just copy paste this function. Used to handle signals
 def init_signal_handler():
-    '''Handles exit signals, User defined signale defined in Base class'''
+    '''Handles exit signals, User-defined signals defined in the Base class'''
     def exit_handler(signalnum, frame):
         config['exit'] = True
     signal.signal(signal.SIGINT, exit_handler)
@@ -262,39 +245,23 @@ def init_signal_handler():
 
 def get_log_level(args):
     '''Log level helper'''
-    levelStr = args.log_level.upper()
-    if levelStr == '0' or levelStr == 'CRITICAL':
-        numeric_log_level = logging.CRITICAL
-    elif levelStr == '1' or levelStr == 'ERROR':
-        numeric_log_level = logging.ERROR
-    elif levelStr == '2' or levelStr == 'WARNING':
-        numeric_log_level = logging.WARNING
-    elif levelStr == '3' or levelStr == 'INFO':
-        numeric_log_level = logging.INFO
-    elif levelStr == '4' or levelStr == 'DEBUG':
-        numeric_log_level = logging.DEBUG
-    else:
-        print(
-            "Could not understand the specified --log-level '%s'" %
-            (args.loglevel))
-        args.print_help()
-        sys.exit(2)
-    return numeric_log_level
-
+    log_level_str = args.log_level.upper()
+    log_level_map = {
+        '0': logging.CRITICAL,
+        '1': logging.ERROR,
+        '2': logging.WARNING,
+        '3': logging.INFO,
+        '4': logging.DEBUG,
+    }
+    return log_level_map.get(log_level_str, logging.INFO)
 
 def main():
-    '''main function'''
+    '''Main function'''
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--log_level",
         default='INFO',
-        help='Specify a log level to use for logging. CRITICAL (0) - \
-                        log only critical errors that drastically affect \
-                        execution ERROR (1) - Log any error in execution \
-                        WARNING (2) - Log all warnings and errors that occur \
-                        INFO (3) - Log informational messages about program \
-                        execution in addition to warnings and errors DEBUG (4) \
-                        - Log debugging information in addition to all')
+        help='Specify a log level to use for logging.')
     parser.add_argument(
         "-p",
         "--port",
@@ -307,22 +274,22 @@ def main():
         default=None,
         help='Path to custom metrics file')
     args = parser.parse_args()
-    # set up logging
+
+    # Set up logging
     os.makedirs('/tmp/moneo-worker', exist_ok=True)
     logging.basicConfig(level=get_log_level(args), filename='/tmp/moneo-worker/moneoExporter.log',
                         format='[%(asctime)s] node_exporter-%(levelname)s-%(message)s')
-    jobId = None  # set a default job id of None
-    # try:
-    init_config(jobId, args.port)
-    init_custom_metrics(args.custom_metrics_file_path)
-    init_signal_handler()
-    init_nvidia_config()
-    init_ib_config()
-    exporter = CustomExporter(args.custom_metrics_file_path)
-    exporter.loop()
-    # except Exception as e:
-    #     logging.error('Raised exception. Message: %s', e)
-
+    job_id = None  # Set a default job id of None
+    try:
+        init_config(job_id, args.port)
+        init_custom_metrics(args.custom_metrics_file_path)
+        init_signal_handler()
+        init_nvidia_config()
+        init_ib_config()
+        exporter = CustomExporter(args.custom_metrics_file_path)
+        exporter.loop()
+    except Exception as e:
+        logging.error('Raised exception. Message: %s', e)
 
 if __name__ == '__main__':
     main()
