f1305c5b26de403bfc0f339e0240e339339b88d7
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2024-03-25 11:58:01 -0400

    Moneo refresh (#79)
    * bug fix for hostfile argument
    
    * template update
    
    * modify start scripts to correct bugs and add worker only launch
    
    modify start scripts to correct bugs and add worker only launch
    
    modify start scripts to correct bugs and add worker only launch
    
    ---------
    
    Co-authored-by: Ubuntu <rafsalas@ub20n8855000001.orxxfqth034upnhnpwma2s5jtc.ax.internal.cloudapp.net>
    Co-authored-by: Ubuntu <rafsalas@nhc-A100vm.dav1onuyw5fejl0wtzkr0deqkf.bx.internal.cloudapp.net>

--- linux_service/start_moneo_services.sh
+++ linux_service/start_moneo_services.sh
@@ -5,6 +5,7 @@
 # Managed Prometheus deployment: ./start_moneo_services.sh 
 # Azure Monitor: ./start_moneo_services.sh azure_monitor
 # Geneva (internal msft): ./start_moneo_services.sh geneva
+# Only start workers: ./start_moneo_services.sh workers
 PublisherMethod=$1 
 
 # Modify as necessary
@@ -20,22 +21,24 @@ fi
 
 procs=("net_exporter" "node_exporter")
 
-if lspci | grep -iq NVIDIA ; then
+if lspci | grep -iq NVIDIA; then
     procs+=("nvidia_exporter")
 fi
 
 if [[ -n $PublisherMethod ]]; then
     if [ "$PublisherMethod" == "geneva" ] || [ "$PublisherMethod" == "azure_monitor" ]; then
         echo "PublisherMethod is valid: $PublisherMethod"
+        procs+=("metrics_publisher")
+    elif [ "$PublisherMethod" == "workers" ]; then
+        echo "Only starting workers"
     else
         echo "PublisherMethod is not one of the valid choices."
         exit 1
     fi
-    procs+=("metrics_publisher")
 fi
 
 function proc_check(){
-    CHECK=`ps -eaf | grep /tmp/moneo-worker/`
+    CHECK=$( ps -eaf | grep /tmp/moneo-worker/)
     WITH_MANAGED_PROM=$1
     for substring in "${procs[@]}"; do
         if [[ $CHECK == *"$substring"* ]]; then
@@ -48,7 +51,7 @@ function proc_check(){
 
     if [[ -n $WITH_MANAGED_PROM && $WITH_MANAGED_PROM = true ]];
     then
-        if [[ $(sudo docker ps -a | grep prometheus) ]] ; then
+        if [[ $(sudo docker ps -a | grep prometheus) ]]; then
             echo "Prometheus docker containers running."
         else
             echo "Prometheus failed to start. Please ensure you have the proper user managed identity assigned to your VMSS/VM."
@@ -68,6 +71,10 @@ sudo systemctl enable moneo@nvidia_exporter.service
 sudo systemctl start moneo@node_exporter.service
 sudo systemctl start moneo@net_exporter.service
 sudo systemctl start moneo@nvidia_exporter.service
+
+if [ "$PublisherMethod" == "workers" ]; then
+    proc_check false
+fi
 
 if [[ -n $PublisherMethod ]]; then
     if [ "$PublisherMethod" == "geneva" ]; then
